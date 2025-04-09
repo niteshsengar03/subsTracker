b802359f3892290e5efe07a88e2b4c638eb26170
Nitesh <niteshsengar9831@gmail.com>
2025-04-10 01:21:38 +0530

    signup controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,46 @@
 import mongoose from "mongoose"
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env";
 
 export const signUp = async (req,res,next)=>{
     // Atomic updates
     const session = await mongoose.startSession();
+    session.startTransaction();
+    try{
+        // create a new user
+        const {name,email,password} = req.body;
+
+        // user already exist
+        const existingUser = await User.findOne(email)
+        if(existingUser==null){
+            // Hash password to store in database
+            const salt = await bcrypt.genSalt(10);
+            const hashPassword = await bcrypt.hash(password,salt);
+            const newUser = await User.create([{name,email,password:hashPassword}],{session}); 
+            const token = jwt.sign({userId: newUser[0]._id},JWT_SECRET,{expiresIn:JWT_EXPIRES_IN});
+        } else{
+            const error =  new Error("User already exists");
+            error.statusCode = 409;
+            throw error; 
+        }
+        session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            success: true,
+            message: 'User created',
+            data:{
+                token,
+                user: newUser[0]
+            }
+        })
+    } catch(er){
+        await session.abortTransaction();
+        session.endSession();
+        next(er);
+    }
 }
 
 export const signIn = async(req,res,next)=>{

f76784c69d6f869000a70e826b126477ed9754e9
Nitesh <niteshsengar9831@gmail.com>
2025-04-14 23:56:33 +0530

    add sign-in feature

--- config/env.js
+++ config/env.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 
 config({path:`.env.${process.env.NOD_ENV ||'development'}.local`});
 
-const PORT=process.env.PORT;
 
+const PORT=process.env.PORT;
 const DB_URL = process.env.DB_URL;
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,26 +4,26 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 
-export const signUp = async (req,res,next)=>{
+export const signUp = async (req, res, next) => {
     // Atomic updates
     const session = await mongoose.startSession();
     session.startTransaction();
-    try{
+    try {
         // create a new user
-        const {name,email,password} = req.body;
+        const { name, email, password } = req.body;
         // user already exist
-        const existingUser = await User.findOne({email});
-        if(existingUser==null){
+        const existingUser = await User.findOne({ email });
+        if (existingUser == null) {
             // Hash password to store in database
             const salt = await bcrypt.genSalt(10);
-            const hashPassword = await bcrypt.hash(password,salt);
-            const newUser = await User.create([{name,email,password:hashPassword}],{session}); 
+            const hashPassword = await bcrypt.hash(password, salt);
+            const newUser = await User.create([{ name, email, password: hashPassword }], { session });
             // console.log(newUser);
-            const token = jwt.sign({userId: newUser[0]._id},JWT_SECRET,{expiresIn:JWT_EXPIRES_IN});
-        } else{
-            const error =  new Error("User already exists");
+            const token = jwt.sign({ userId: newUser[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+        } else {
+            const error = new Error("User already exists");
             error.statusCode = 409;
-            throw error; 
+            throw error;
         }
         await session.commitTransaction();
         session.endSession();
@@ -31,23 +31,51 @@ export const signUp = async (req,res,next)=>{
         res.status(201).json({
             success: true,
             message: 'User created',
-            data:{
+            data: {
                 token,
                 user: newUser[0]
             }
         })
-    } catch(er){
-        if(session.inTransaction())
+    } catch (er) {
+        if (session.inTransaction())
             await session.abortTransaction();
         session.endSession();
         next(er);
     }
 }
 
-export const signIn = async(req,res,next)=>{
+export const signIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        // user doesnot exist in database
+        if (!user) {
+            const error = new Error("User doest not exist");
+            error.statusCode = 404;
+            throw error;
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            const error = new Error("Password in incorrect");
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET);
 
+        return res.status(200).json({
+            success: true,
+            message: 'User signed in',
+            data: {
+                token,
+                user
+            }
+        });
+    }
+    catch (err) {
+        next(err);
+    }
 }
 
-export const signOut = async(req,res,next)=>{
+export const signOut = async (req, res, next) => {
 
 }
